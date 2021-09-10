05b5d5ba1ab7df0ff05b931102d4da8daf9dffc0
Kritarth Sharma <kritarth2121@gmail.com>
2021-09-10 23:23:02 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,16 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOp4BxO9VOATAB22wPJDyOFUhj-lh9f2c",
-  authDomain: "amz-clone-b9eab.firebaseapp.com",
-  databaseURL: "https://amz-clone-b9eab.firebaseio.com",
-  projectId: "amz-clone-b9eab",
-  storageBucket: "amz-clone-b9eab.appspot.com",
-  messagingSenderId: "287079566892",
-  appId: "1:287079566892:web:f06f6fd05fd1b491a30b89",
-  measurementId: "G-W8LZDEYFJQ",
+
+  
+  apiKey: "AIzaSyAeS8DlANWm-iWi40u-9iM50084OXiNHhs",
+  authDomain: "clone-1ff2d.firebaseapp.com",
+  databaseURL: "https://clone-1ff2d-default-rtdb.firebaseio.com/",
+  projectId: "clone-1ff2d",
+  storageBucket: "clone-1ff2d.appspot.com",
+  messagingSenderId: "12389972648",
+  appId: "1:12389972648:web:44daa17832c392c506609d",
+  measurementId: "G-1Q8D0J75RB"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
